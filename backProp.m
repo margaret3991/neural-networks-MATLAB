cd8ce854b3d7f039c5284f4aa4f37c191f80a615
Luna (old mac) <Luna@Margarets-Mac-mini.local>
2020-02-23 01:12:57 -0800

    Adding new backprop file

---
+++ backProp.m
@@ -0,0 +1,19 @@
+function [ mse ] = backProp(trainInputs,trainTargets, learningRate, iterations, architecture)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+%   trainInputs = input vector for training set 
+%   trainTargets = target vector for training set
+%   learning rate = alpha learning rate 
+%   iterations = set value for maximum iterations 
+%   architecture = vector representing number of neurons and layers for the
+%       neural network i.e. [2 1] for a 1-2-1 pattern (doesn't count input)
+
+%   transfer function definitions:
+%   layer1 function: logsigmoid()
+%   layer2 function: linear() 
+
+
+
+outputArg2 = inputArg2;
+end
+
